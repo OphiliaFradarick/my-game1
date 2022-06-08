58a1de3ab0a32f9cde4dfd7c0f4927f049da1304
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2022-06-08 16:52:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var ground
 var backgroundimage,zombieimage,hunterimage,bulletimage
-
+var bulletArray=[]
 
 function preload(){
 backgroundimage=loadImage("assets/background.png")
@@ -18,24 +18,47 @@ ground.addImage(backgroundimage)
 ground.scale=7
 ground.velocityX=-2
 
+bullet=createSprite(326,375,5,5)
+bullet.addImage(bulletimage)
+bullet.scale=0.07
+bullet.debug = true;
+
+
 hunter=createSprite(200,windowHeight-200,100,100)
 hunter.addImage(hunterimage)
 hunter.scale=1.5
+hunter.debug = true;
+hunter.setCollider("rectangle",-50,0,90,200)
 
-bullet=createSprite(400,400,5,5)
-bullet.addImage(bulletimage)
-bullet.scale=0.3
-
-zombie=createSprite(600,600,50,50)
-zombie.addAnimation(zombieimage)
 }
 
 function draw(){
- background("black")
+    background("black")
 
- if(ground.x<windowWidth/2-100){
-    ground.x=windowWidth/2+200
+    if(ground.x<windowWidth/2-100){
+        ground.x=windowWidth/2+200
+    }
+
+    multipleZombies();
+
+    //fire multiple bullets on clicking space bar using for loop
+    //for(var i=0;i<bullets.length;i++){
+      //
+    //}
+
+    drawSprites()
 }
 
-drawSprites()
+//function for multiple zombies
+function multipleZombies(){
+   if(frameCount%60===0){
+    var zombie=createSprite(windowWidth,windowHeight-190,10,10)
+    zombie.addAnimation("Image_zombie",zombieimage)
+    zombie.scale=3;
+    zombie.debug = true;
+    zombie.setCollider("rectangle",-13,0,30,100)
+    zombie.velocityX=-6
+    zombie.lifetime=windowWidth/6
+    zombie.depth=zombie.depth+1
+   }
 }
